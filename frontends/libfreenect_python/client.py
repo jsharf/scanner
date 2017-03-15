c3a9d50d65262d5760d964b806a666a4a2de866e
Jacob Sharf <jacobsharf@gmail.com>
2017-03-14 23:28:06 -0700

    Client updated to be async. Still need to improve proto performance. finishing work for the night

--- frontends/libfreenect_python/client.py
+++ frontends/libfreenect_python/client.py
@@ -3,6 +3,9 @@ import freenect
 import cv2
 import numpy as np
 import meshbuilder_pb2
+import meshbuilder_pb2_grpc
+import grpc
+import sys
 
 #function to get RGB image from kinect
 def get_video():
@@ -16,31 +19,95 @@ def get_depth():
   array = array.astype(np.uint8)
   return array
 
+def display_depth(dev, data, timestamp):
+    global keep_running
+    global depth
+    depth = data.astype(np.uint8)
+    cv2.imshow('Depth', depth)
+    if cv2.waitKey(10) == 27:
+        keep_running = False
+  
+frames = []
+keep_running = True
+depth = np.array([1])
+last_sent_depth = np.array([1])
+project_name = ""
+stub = None
+
+
+def loop(*args):
+  global stub
+  global depth
+  global last_sent_depth
+  global keep_running
+  global frames
+  global project_name
+
+  if not keep_running:
+    raise freenect.Kill
+
+  if ((depth == last_sent_depth).all()):
+    return
+
+  print("New depth value!")
+
+  # Stuff frame in proto.
+  proto = meshbuilder_pb2.AddRequest()
+  proto.name = project_name
+  for row in depth:
+    new_row = proto.depth.rows.add()
+    new_row.values[:] = row
+  proto.depth.x_fov = 58.5
+  proto.depth.y_fov = 46.6
+  
+  print("depth_in_proto")
+
+  # Start GRPC
+  frames.append(stub.Add.future(proto))
+  last_sent_depth = depth
+
+  print("proto_sent_to_grpc")
+
+  # Reap a frame. Don't spent time going through all of them...
+  for i,_ in enumerate(frames):
+    if frames[i].done():
+      if (frames[i].exception() != None):
+        print("Add grpc returned exception: {0}".format(str(frames[i].exception())))
+      del frames[i]
+      break
+  
+  print("rpc_frames_handled")
+
 def main():
-  depth = get_depth()
+  global frames
+  global project_name
+  global stub
+  if (len(sys.argv) != 3):
+    print("Usage: python client.py address:port project_name")
+    sys.exit(1)
 
-  while (cv2.waitKey(10) != 32):
-    #get a frame from depth sensor
-    depth = get_depth()
-    #display depth image
-    cv2.imshow('stream',depth)
+  server = sys.argv[1]
+  project_name = sys.argv[2]
 
-  first_point_cloud = depth
-  cv2.imshow('first', first_point_cloud)
+  channel = grpc.insecure_channel(server)
+  stub = meshbuilder_pb2_grpc.MeshBuilderStub(channel)
 
-  while (cv2.waitKey(10) != 32):
-    #get a frame from depth sensor
-    depth = get_depth()
-    #display depth image
-    cv2.imshow('stream',depth)
+  print("Creating Project.")
 
-  second_point_cloud = depth
+  # Create the project (if it doesn't already exist) and get the project ID.
+  create_project_proto = meshbuilder_pb2.CreateProjectRequest()
+  create_project_proto.name = project_name
+  stub.CreateProject(create_project_proto)
+  
+  print("Project created!.")
 
-  cv2.imshow('second', second_point_cloud)
 
-  # quit program when 'esc' key is pressed
-  while (cv2.waitKey(0) != 27):
-    continue
+  print("Streaming frames...")
+
+  print('Press ESC in window to stop')
+  freenect.runloop(depth=display_depth,
+                   video=None,
+                   body=loop)
 
   cv2.destroyAllWindows()
 
